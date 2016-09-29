f6d903412994c89cc321d25f75e14e104601b41b
Tyler Wilgenbusch <tjw8700@rit.edu>
2016-09-29 19:17:18 -0400

    Finished implmentation of new zipunzipper script to work with new myCourses format

--- zipunzipper.py
+++ zipunzipper.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python
 
+"""
+zipunzipper.py - Given a zip directory of student submissions pulled in
+    bulk from myCourses, unzips them and puts them in directories with the
+    students' user names.
+
+Author: T. Wilgenbusch
+"""
+
 import sys
 import os.path
 
@@ -9,11 +17,11 @@ from os import listdir
 #place to write all output we don't want to see
 devnull = open(os.devnull, 'w')
 
-def get_student_dict(student_name_file, file_list):
+def get_student_dict(student_name_file, file_list, temp_zip_directory_name):
     """
     get_student_dict - given a file containing student usernames and full names, 
-    will return a dictionary that maps student user names to file names in 
-    list of files
+        will return a dictionary that maps student user names to file names in 
+        list of files
 
     Final dictionary: (key, value) -> (username, {file1, file2})
 
@@ -24,89 +32,91 @@ def get_student_dict(student_name_file, file_list):
     # Line format in student_name_file:
     # username firstName lastName
 
-    # (key, value) -> ([firstName, lastName], username)
+    # (key, value) -> (firstNamelastName, username)
     username_dict = {}
+
+    # (key, value) -> ([file1, file2])
     student_name_dict = {}
 
     for line in open(student_name_file):
         items = line.split(" ")
-        username_dict[[items[1], items[2]]] = items[0] 
+
+        username_dict[items[1].strip()+items[2].strip()] = items[0].strip()
         student_name_dict[items[0]] = []
 
     # File format for files in file_list: 
     # DDDDD-DDDDDDD - lastName, firstName - fileName
+    for current_file_name in file_list:
+
+        file = temp_zip_directory_name + "/" + current_file_name
+
+        full_name = (file.split(" - ")[1].strip()).split(", ")
+        first_name, last_name = full_name[1].strip(), full_name[0].strip()
+
+        # Grab the original name of the file and rename the current one
+        file_name = file.split(" - ")[2].strip()
+
+        user_name = username_dict[first_name+last_name]
+
+        call(["mkdir", file, temp_zip_directory_name + "/" + user_name ], stdout=devnull, stderr=devnull)
+        call(["mv", file, temp_zip_directory_name + "/" + user_name + "/" + file_name], stdout=devnull, stderr=devnull)
+
+
+        student_name_dict[user_name] += [user_name + "/" + file_name]
 
     return student_name_dict
 
 
+def move_labs(student_dict, dir_name, temp_zip_directory_name):
+    """
+    move_labs - moves all of the files from in the temporary directory to the 
+        provided directory, creating any student directories if they do 
+        not already exist
 
-def create_dirs_is_nonexistent(student_dict, root_dir):
-    """
-    creates the student directory structure based on the student usernames 
-    passed in if it doesn't already exist
-    @param student_dict the dictionary that contains all the student usernames
-    @param root_dir the parent directory to put all of the student directories
-    """
-    if(not root_dir.endswith('/')):
-        root_dir += '/'
-    for student in student_dict.keys():
-        if(not os.path.exists(root_dir + student)):
-            call(["mkdir", root_dir + student], stdout=devnull, stderr=devnull)
-
-def unzip_labs(student_dict, dir_name, temp_zip_directory_name):
-    """
-    unzips all of the lab zip files and places them in the correct directories
-    will warn the user if it was unable to get certain labs for certain files
     @param student_dict the dictionary of students
     @param dir_name the directory to unzip all of the labs into
     @param temp_zip_directory_name name of directory with temp labs
     """
-    for student in student_dict.keys():
-        for file in student_dict[student]:
-            #if there are more than one labs submitted, we want to save the in seperate directories
-            if(len(student_dict[student]) > 1):
-                first_split = file.split(" - ")
-                if(len(first_split)  > 2):
-                    file_directory_name = first_split[2]
-                    second_split = file_directory_name.split(".zip")
-                    if(len(second_split) > 0):
-                        file_directory_name = second_split[0]
-                        call(["mkdir", dir_name + student + '/' + file_directory_name], stdout=devnull, stderr=devnull)
-                        call(["unzip", "-d", dir_name + student +  '/' + file_directory_name, temp_zip_directory_name + '/' + file], stdout=devnull, stderr=devnull)
-                    else:
-                        print("ERROR: Unable to unzip lab: " + file + "for student: " + student)
-                else:
-                    print("ERROR: Unable to unzip lab: " + file + "for student: " + student)
-            #otherwise they only have one lab submitted and we should just unzip it here
-            else:
-                call(["unzip", "-d", dir_name + student, temp_zip_directory_name + '/' + file], stdout=devnull, stderr=devnull)
-        
-        if(len(student_dict[student]) == 0):
-            print("WARNING: Could not find labs for student: " + student)
+    for user_name in student_dict.keys():
+
+        # Make the student directory
+        if not os.path.exists(dir_name + "/" + user_name):
+            call(["mkdir", dir_name + "/" + user_name ], 
+            stdout=devnull, stderr=devnull)
+
+        # Directory to put the submision into to avoid mixing with 
+        # try submissions directories that are already there
+        call(["mkdir", dir_name + "/" + user_name + "/Submission"],
+            stdout=devnull, stderr=devnull)
+
+        # NOTE: There may be no files to move
+        for file in student_dict[user_name]:
+            call(["mv", temp_zip_directory_name + "/" + file, 
+                dir_name + "/" + user_name + "/Submission"],
+                stdout=devnull, stderr=devnull)
 
 def usage():
     """
     prints the usage statement for the script
     """
+
     print('usage: zipunzipper.py zipfile students dirname')
     print('     zipfile  - The zip file downloaded from myCourses that contains all the labs')
-    print('     students - file containing all the students name (new line delimited).')
+    print('     students - file containing all the students full names and usernames (new line delimited).')
     print('     dirname  - the name of the directory to drop all of the labs (i.e ./course/labs/lab01/')
 
 def main():
     """
-    unzips a zip file that contains zip files from dropbox and puts them 
-    in the correct student directory
+    unzips a zip file that contains all the from dropbox and puts them 
+        in the correct student directory
+    
     first argument is the zip file
     second argument is a list of the students (directories) names
     third argument is the Lab directory
     """
-    
-    print(len(sys.argv))
-    print(sys.argv)
 
     # Check for valid number of parameters
-    if(len(sys.argv) == 4):
+    if(len(sys.argv) != 4):
         usage()
         exit(1)
 
@@ -138,33 +148,30 @@ def main():
         dir_name = dir_name + '/'
 
     call(["mkdir", temp_zip_directory_name], stdout=devnull, stderr=devnull)
-    call(["unzip", "-d", temp_zip_directory_name, zip_file_name], stdout=devnull, stderr=devnull)
+    call(["unzip", "-d", temp_zip_directory_name, zip_file_name], 
+        stdout=devnull, stderr=devnull)
 
     # New file format: 
     # DDDDD-DDDDDDD - lastName, firstName - fileName
 
-    #grab all of the zip files that were unzipped from dropbox super zip
-    # TODO: Change this to reflect a file list
+    # Grab all of the zip files that were unzipped from dropbox super zip
     file_list = listdir(temp_zip_directory_name)
 
+    # Remove the "index.html" file from the file_list
+    file_list.remove("index.html")
+
     #create the student dictionary 
-    student_dict = get_student_dict(student_name_file, file_list)
-    
+    student_dict = get_student_dict(student_name_file, file_list, temp_zip_directory_name)
+    sorted_keys = sorted(student_dict.keys())
 
-    # TODO: Change to point individual file sin a flat directory to a username
-    # dictionary
-    for student in student_dict.keys():
-        for file in output:
-            if student in file:
-                student_dict[student] += [file]
+    # Print out all files and usernames
+    for username in sorted_keys:
+        print username,
+        print(student_dict[username])
 
-    #create student directories
-    create_dirs_is_nonexistent(student_dict, dir_name)
+    move_labs(student_dict, dir_name, temp_zip_directory_name)
 
-    #create the student lab directories (could be more)
-    unzip_labs(student_dict, dir_name, temp_zip_directory_name)
-
-    #remove the temp directory
+    # Remove the temp directory
     call(["rm", "-r", temp_zip_directory_name], stdout=devnull, stderr=devnull)
 
 main()
